144474b049e9f232323934c5e6555fc4ba06666f
Naeema <naeemaadenike@gmail.com>
2025-08-27 13:06:34 +0100

    new

--- week_01/mixing_data_types.py
+++ week_01/mixing_data_types.py
@@ -1,5 +1,5 @@
 age = int(input("Your age:"))
 height = float(input("Your height:"))
-name = input("Your name:A")
+name = input("Your name:")
 
 print(f"My name is {name}, I'm {age} years old. I am {height} tall.")
